817b63512c10528816ab69ef0bee2d3bf5487f3f
alfonsog0 <alfonsogambino@protonmail.com>
2025-11-20 12:24:25 +0000

    feat (scrabble): add tile_score(char) function for letter scoring

--- scrabble/scrabble.cpp
+++ scrabble/scrabble.cpp
@@ -8,3 +8,26 @@
 using namespace std;
 
 /* insert your function definitions here */
+int tile_score(char letter) {
+    switch (toupper(letter)) {
+    case 'A': case 'E': case 'I': case 'L': case 'N': case 'O': case 'R': case 'S': case 'T': case 'U':
+        return 1;
+    case 'D': case 'G':
+        return 2;
+    case 'B': case 'C': case 'M': case 'P':
+        return 3;
+    case 'F': case 'H': case 'V': case 'W': case 'Y':
+        return 4;
+    case 'K':
+        return 5;
+    case 'J': case 'X':
+        return 8;
+    case 'Z': case 'Q':
+        return 10;
+    case ' ': case '?':
+        return 0;    
+    default:
+        return -1;
+    }
+}
+
