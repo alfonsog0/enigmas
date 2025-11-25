c2663db77bb0874a2fd37e021eb162c4e2276bf7
alfonsog0 <alfonsogambino@protonmail.com>
2025-11-25 12:52:05 +0100

    feat (doublests): valid_step function to chek id a step follows the rules and makefile

--- doublets/doublets.cpp
+++ doublets/doublets.cpp
@@ -23,3 +23,26 @@ bool dictionary_search(const char *word) {
 }
 
 /* add your function definitions here */
+bool valid_step(const char* current_word, const char* next_word) {
+   // rule 2: only one letter changes
+   // same lenght
+   size_t len_current = strlen(current_word);
+   size_t len_next = strlen(next_word);
+   if (len_current != len_next) return false;
+
+   // one letter changes
+   const char* current_word_ptr = current_word;
+   const char* next_word_ptr = next_word;
+   int divergencies = 0;
+   while (*current_word_ptr != '\0') {
+      if (*current_word_ptr != *next_word_ptr) divergencies++;
+      current_word_ptr++;
+      next_word_ptr++;
+   }
+   if (divergencies != 1) return false;
+
+   // rule 4: next_word must be present in the dict
+   if (!dictionary_search(next_word)) return false;
+
+   return true;
+}
