311e55d4fdf368d7c424c58f1cc2588c8f08925a
alfonsog0 <alfonsogambino@protonmail.com>
2025-12-11 16:43:32 +0100

    feat (spell): add frequency(const char*) returns the frequency if a word in words.dat

--- spell/spell.cpp
+++ spell/spell.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include "spell.h"
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 /* insert your function definitions here */
+int frequency(const char* target) {
+    // open words.dat
+    ifstream input("words.dat");
+    string word;
+    int number;
+
+    // loop through the file getting the lines
+    while (input >> number >> word) {
+        // check if the sencond word in the line matches the target
+        if (word == target) {
+            // if it does return the first part of the line
+            return number;
+        }   
+    }
+    // loop reads every line without finding target --> return 0
+    return 0;
+}
--- spell/spell.h
+++ spell/spell.h
@@ -2,3 +2,9 @@
 #define MAX_LENGTH 512
 
 /* insert your function prototypes here */
+
+/* takes as its parameter a target
+word and which returns the number of times the target word appears in the corpus (by
+looking this up in words.dat). If the word cannot be found in words.dat then the function
+should return 0.*/
+int frequency(const char* target);
